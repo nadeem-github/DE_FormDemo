57506aa312bd2e9946f6a96edeb7166c703dba6a
Rupesh <rupeshpantawane62@gmail.com>
2025-04-08 10:53:50 +0530

    .

--- backend/routes/admin.router.js
+++ backend/routes/admin.router.js
@@ -6,10 +6,7 @@ const passport = require("passport");
 require("../middleware/passport")(passport);
 const adminMidd = require('../middleware/admin.middleware');
 const AuthController = require("../controllers/admin/auth.controller");
-// const TaskController = require("../controllers/admin/task.controller");
-// const UserRelationController = require("../controllers/admin/user_relation.controller")
-
-
+const VertualRemebranceController = require("../controllers/admin/virtual_remebrance.controller");
 // user authentication AuthController
 adminRouter.post("/login", AuthController.login);
 adminRouter.post("/create-mock", AuthController.createMock);
@@ -20,16 +17,10 @@ adminRouter.post("/delete-mock", AuthController.deleteMock);
 adminRouter.post("/delete-selected-mock", AuthController.deleteSelectedMock);
 adminRouter.post("/download-excel", AuthController.downloadMock);
 adminRouter.post("/update-mock", AuthController.updateMock);
-// adminRouter.post("/register",passport.authenticate("jwt", { session: false }), adminMidd.adminUser, AuthController.Register);
-// adminRouter.post("/fetch-user", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, AuthController.fetchUser);
-// adminRouter.post("/update-user", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, AuthController.updateRegister);
-// adminRouter.post("/delete-user", passport.authenticate("jwt", { session: false }), adminMidd.checkUser, AuthController.deleteUser);
-// adminRouter.post("/get-users",  TaskController.getUsers);
-// adminRouter.post("/create-user",  TaskController.createUser);
-// adminRouter.post("/get-login-user",  UserRelationController.getUserLogin);
-// adminRouter.post("/create-user-relation",  UserRelationController.createUserRelation);
-// adminRouter.post("/update-user-relation",  UserRelationController.updateUserRelation);
-// adminRouter.post("/get-user-relation",  UserRelationController.getUserRelation);
+
+adminRouter.post("/create-remebrance", VertualRemebranceController.createRmembrance);
+adminRouter.post("/fetch-remebrance", VertualRemebranceController.fetchRmembrance);
+
 
 
 
